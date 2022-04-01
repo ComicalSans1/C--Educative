cf63b93984c188f97485b0a076b1ed90931b20dc
Sans <hewhototallymustnotbenamed@gmail.com>
2022-04-01 18:19:00 +0530

    spere

---
+++ basics/spherearea.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(void){
+    double r = 10.9;
+    const double pi = M_PI;
+    double area = 4 * pi * r * r;
+    cout << area;
+}
