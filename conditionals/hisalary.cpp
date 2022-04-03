f284827d76872d7610a42f47a11b86b57432fe2a
Sans <hewhototallymustnotbenamed@gmail.com>
2022-04-03 12:41:27 +0530

    salar

---
+++ conditionals/hisalary.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int salary1 = 56000;
+    int salary2 = 89000;
+
+    if(salary1 > salary2) {cout << "person1";}
+    else{cout << "person2";}
+}
